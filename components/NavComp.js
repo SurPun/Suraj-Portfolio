af15f869613579fd35f65552c7cbd245e3f4431a
Suraj Pun <s_pun@outlook.com>
2022-12-20 16:05:30 +0000

    little card fix and responsive navbar

--- components/NavComp.js
+++ components/NavComp.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
 
 export default function NavComp() {
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
   return (
     <>
-      <ul class="nav-container">
-        <li>
-          <a>Home</a>
-        </li>
-        <li>
-          <a>About</a>
-        </li>
-        <li>
-          <a>Work</a>
-        </li>
-      </ul>
+      <div class="topnav" id="myTopnav">
+        {/* Home */}
+        <a href="#home" class="active">
+          Home
+        </a>
+
+        {/* About */}
+        <a href="#about">About</a>
+
+        {/* Work */}
+        <a href="#work">Work</a>
+
+        {/* Contact */}
+        <a href="#contact">Contact</a>
+
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          {/* Hamburger */}
+          <FaBars />
+        </a>
+      </div>
     </>
   );
 }
