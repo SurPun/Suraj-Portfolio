5b6a9f1b09cab3f346408cd4898fe0caff798c7d
Suraj Pun <s_pun@outlook.com>
2022-12-21 16:47:19 +0000

    Added responsive NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/Main.scss";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
