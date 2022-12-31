f672996cc6c42872c0b0055b67e9b58e250d9be5
Suraj <S_pun@outlook.com>
2022-12-31 22:03:12 +0000

    About page

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="center">
+    <div className="aboutDiv">
       <h2>About Page</h2>
 
-      <p>Work in Progress</p>
+      <h3>Welcome to my software development portfolio!</h3>
+
+      <p>
+        I recently completed the Founders and Coders bootcamp, a highly
+        regarded, intensive program that trains individuals to become full-stack
+        web developers. Throughout the course, I gained a solid foundation in
+        modern web development technologies and practices, including HTML, CSS,
+        JavaScript, Node.js, and React.
+      </p>
     </div>
   );
 };
