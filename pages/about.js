d7781caaa5711293d060320626c3bb52c030f1a6
Suraj <S_pun@outlook.com>
2022-12-31 22:08:31 +0000

    About Page

--- pages/about.js
+++ pages/about.js
@@ -21,6 +21,21 @@ const About = () => {
         comfortable working in a team environment and have experience
         collaborating on projects using agile development methodologies.
       </p>
+
+      <p>
+        During the bootcamp, I had the opportunity to work on a number of
+        real-world projects, including a web application for organizing and
+        managing events, a social media platform for posting song recommendations, and an Immersive empathy training web application. These projects allowed me to apply my skills and knowledge in a
+        practical setting and demonstrate my ability to deliver high-quality software.
+      </p>
+
+      <p>
+        I am now excited to take my skills and passion for software development
+        to the next level. I am eager to find new challenges and opportunities
+        to grow as a developer and contribute to innovative projects. If you
+        have a project that could benefit from my skills and enthusiasm, please
+        don't hesitate to reach out.
+      </p>
     </div>
   );
 };
